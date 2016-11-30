88aa417d9926b9fa979c114bc65f02ca7607eb6f
Jacqueline Lo <jaml@linux.ucla.edu>
2016-11-29 22:22:01 -0800

    Add merging to part 2 question 2 script

---
+++ part2/part2q2_run_merge.sh
@@ -0,0 +1,19 @@
+# Part 2, question 2 script
+
+# try running this 3 times so we get better representation of data
+for((i=1; i<=3; i++)); do
+	# for each of the available strides
+	for stride in -1 1 -2 2 4 -4 8 -8 16 -16; do
+		cmd64="/home/jaml/Xmem/bin/xmem-linux-x64 -c64 -w4 -j1 -s -S$stride -v -f part2q2_c64_stride${stride}_run${i}.csv > part2q2_c64_${stride}_run${i}.out"
+		cmd32="/home/jaml/Xmem/bin/xmem-linux-x64 -c32 -w4 -j1 -s -S$stride -v -f part2q2_c32_stride${stride}_run${i}.csv > part2q2_c32_${stride}_run${i}.out"
+		eval $cmd64
+		eval $cmd32
+	done
+done
+
+# merge all chunk size 64 run results
+head -1 part2q2_c64_1_run1.csv > part2q2_c64_3runs_merged.csv
+for filename in $(ls part2q2_c64_*_run?.csv); do sed 1d $filename >> part2q2_c64_3runs_merged.csv; done
+# merge all chunk size 32 run results
+head -1 part2q2_c32_1_run1.csv > part2q2_c32_3runs_merged.csv
+for filename in $(ls part2q2_c32_*_run?.csv); do sed 1d $filename >> part2q2_c32_3runs_merged.csv; done
